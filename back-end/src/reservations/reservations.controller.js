040aad4bf6012d28c3d9abaa53a5cccaccac5483
Tyler Forbes <forbes2121@gmail.com>
2022-08-17 22:31:16 -0400

    commit for U3 story on both back end & front end

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,99 +1,43 @@
 /**
  * List handler for reservation resources
  */
-//const { whereNotExists } = require("../db/connection");
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-const service = require("./reservations.service");
-const theValidator = require("../../../front-end/src/utils/validateTest");
-//const { isPast, isTuesday } = require("../../../front-end/src/utils/validate");
 
-async function list(req, res) {
-	const reservations = await service.listByDate(req.query.date);
-	res.json({
-		data: [...reservations],
-	});
-}
-function compareKeys(a, b) {
-	const aKeys = Object.keys(a).sort();
-	const bKeys = Object.keys(b).sort();
-	return JSON.stringify(aKeys) === JSON.stringify(bKeys);
-}
-async function validate(req, res, next) {
-	const added = req.body.data;
-	if (!added)
-		next({
-			status: 400,
-			message:
-				"Invalid data format provided. Requires {string: [first_name, last_name, mobile_number], date: reservation_date, time: reservation_time, number: people}",
-		});
-	const test = {
-		first_name: null,
-		last_name: null,
-		mobile_number: null,
-		reservation_date: null,
-		reservation_time: null,
-		people: 0,
-	};
-	//console.log(added);
-	if (compareKeys(added, test)) {
-		for (let theValue in added) {
-			if (theValue === `people`) {
-				console.log(added[theValue]);
-				if (typeof added[theValue] != `number`) {
-					return next({
-						status: 400,
-						message: "people must be a valid number",
-					});
-				}
-			}
-			if (theValue === `reservation_date`) {
-				if (!/\d{4}-\d{2}-\d{2}/.test(added[theValue])) {
-					return next({
-						status: 400,
-						message: "reservation_date must be a valid date",
-					});
-				}
-				if (theValidator.isPast(added[theValue]))
-					return next({
-						status: 400,
-						message: "reservation_date must be a date in the future",
-					});
-				if (theValidator.isTuesday(added[theValue]))
-					return next({
-						status: 400,
-						message:
-							"reservation_date must not be a Tuesday, we're closed that day, sorry!",
-					});
-			}
-			if (
-				theValue === `reservation_time` &&
-				!/[0-9]{2}:[0-9]{2}/.test(added[theValue])
-			) {
-				return next({
-					status: 400,
-					message: "reservation_time must be a valid time",
-				});
-			}
-			if (!added[theValue])
-				return next({
-					status: 400,
-					message:
-						"Invalid data format provided. Requires {string: [first_name, last_name, mobile_number], date: reservation_date, time: reservation_time, number: people}",
-				});
-		}
-		return next();
-	}
-	next({
-		status: 400,
-		message:
-			"Invalid data format provided. Requires {string: [first_name, last_name, mobile_number], date: reservation_date, time: reservation_time, number: people}",
-	});
-}
-async function create(req, res) {
-	const added = await service.create(req.body.data);
-	res.status(201).json({ data: added });
-}
-module.exports = {
-	list: asyncErrorBoundary(list),
-	create: [asyncErrorBoundary(validate), asyncErrorBoundary(create)],
-};
+ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+ const service = require("./reservations.service");
+ const { theValidator } = require("../../../front-end/src/utils/validateTest");
+ 
+ async function list(req, res) {
+   const reservations = await service.listByDate(req.query.date);
+   res.json({
+     data: [...reservations],
+   });
+ }
+ async function validate(req, res, next) {
+   const added = req.body.data;
+   let message;
+   function setError(err) {
+     if (err) message = err.message;
+   }
+   if (!added)
+     return next({
+       status: 400,
+       message:
+         "Invalid data format provided. Requires {string: [first_name, last_name, mobile_number], date: reservation_date, time: reservation_time, number: people}",
+     });
+   if (!theValidator(added, setError) || typeof added.people != "number") {
+     if (!message) {
+       message = "people must be a number";
+     }
+     return next({ status: 400, message });
+   } else {
+     return next();
+   }
+ }
+ async function create(req, res) {
+   const added = await service.create(req.body.data);
+   res.status(201).json({ data: added });
+ }
+ module.exports = {
+   list: asyncErrorBoundary(list),
+   create: [asyncErrorBoundary(validate), asyncErrorBoundary(create)],
+ };
--- front-end/src/ReservationForm.jsx
+++ front-end/src/ReservationForm.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { createReservations } from "./utils/api";
 import { useHistory } from "react-router-dom";
 import ErrorAlert from "./layout/ErrorAlert";
-import { previous } from "./utils/date-time";
-import { mobileValidate, isTuesday, isPast } from "./utils/validate";
+
+const { mobileValidate, theValidator } = require("./utils/validateTest");
 export default function ReservationForm() {
 	const history = useHistory();
 	const [formData, setFormData] = useState({
@@ -22,25 +22,7 @@ export default function ReservationForm() {
 			phoneNumber = mobileValidate(phoneNumber, formData.mobile_number.length);
 			event.target.value = phoneNumber;
 		}
-		if (event.target.id === `reservation_date`) {
-			let bad = false;
-			let message = "";
-			if (isTuesday(event.target.value)) {
-				message += "  /Closed Tuesdays";
-				bad = true;
-			}
-			if (isPast(event.target.value)) {
-				message += " /Must be in Future";
-				bad = true;
-			}
-			if (bad) {
-				if (error) setError(new Error(message + error.message));
-				else setError(new Error(message));
-				event.target.value = null;
-			} else {
-				setTimeout(setError(null), 5000);
-			}
-		}
+
 		changeObj[event.target.id] = event.target.value;
 		changeObj.people = Number(changeObj.people);
 		setFormData(changeObj);
@@ -49,11 +31,16 @@ export default function ReservationForm() {
 	const theSubmit = (event) => {
 		event.preventDefault();
 		const abortController = new AbortController();
-		createReservations(formData, abortController.signal)
-			.then(() => history.push(`/dashboard?date=${formData.reservation_date}`))
-			.catch((err) => {
-				setError(err);
-			});
+
+		if (theValidator(formData, setError)) {
+			createReservations(formData, abortController.signal)
+				.then(() =>
+					history.push(`/dashboard?date=${formData.reservation_date}`)
+				)
+				.catch((err) => {
+					setError(err);
+				});
+		}
 	};
 
 	return (
@@ -103,7 +90,6 @@ export default function ReservationForm() {
 						onChange={formChange}
 						value={formData.reservation_date}
 						type="date"
-						//min={previous(new Date().toISOString().split("T")[0])}
 						placeholder="YYYY-MM-DD"
 						pattern="\d{4}-\d{2}-\d{2}"
 						className="form-control"
@@ -120,7 +106,6 @@ export default function ReservationForm() {
 						placeholder="HH:MM"
 						pattern="[0-9]{2}:[0-9]{2}"
 						className="form-control"
-						required
 					/>
 					<label htmlFor="people">Party Size</label>
 					<input
