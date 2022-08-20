0e93bdd74e39ba66fd73cb1788e9c009738ea1a8
Tyler Forbes <forbes2121@gmail.com>
2022-08-20 11:33:11 -0400

    trying to get migration working for status in BE

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-	return knex.schema.createTable("reservations", (table) => {
+	return knex.schema.table("reservations", (table) => {
 		table.increments("reservation_id").primary();
 		table.string("first_name").notNullable();
 		table.string("last_name").notNullable();
