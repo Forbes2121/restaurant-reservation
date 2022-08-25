652b3b5f3c07c15bcaf8b16d1d1cb1c309f4bfbb
Tyler Forbes <forbes2121@gmail.com>
2022-08-24 23:34:41 -0400

    hard coded backend deployed url into frontend

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,8 +5,8 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
-//"https://forbes-backend-app.herokuapp.com";
+const API_BASE_URL = "https://forbes-backend-app.herokuapp.com";
+//process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
 
 
 /**
